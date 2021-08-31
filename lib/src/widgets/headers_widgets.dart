b954dc1ac80bb0a32eb40931af8d6f2ea5d325fe
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-08-30 22:41:09 -0300

    Header ola

--- lib/src/widgets/headers_widgets.dart
+++ lib/src/widgets/headers_widgets.dart
@@ -36,7 +36,7 @@ class HeaderDiagonal extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       child: CustomPaint(
-        painter: _HeaderCurvoPainter(),
+        painter: _HeaderWavePainter(),
       ),
     );
   }
@@ -151,6 +151,38 @@ class _HeaderCurvoPainter extends CustomPainter{
     
     path.lineTo(0, size.height * 0.25);
     path.quadraticBezierTo(size.width * 0.5, size.height * 0.4, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    
+
+    canvas.drawPath(path, lapiz);
+    
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+
+    return true;
+  }
+
+}
+
+class _HeaderWavePainter extends CustomPainter{
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final lapiz = Paint();
+    //Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 20;
+
+    final path = Path();
+    //dibujar Path y lapiz
+    
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.4, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.1, size.width, size.height * 0.25);
     path.lineTo(size.width, 0);
     
 
