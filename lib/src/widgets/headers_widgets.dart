3e65bf58bb5ff768159544c51320b04396825ecd
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-08-30 22:52:36 -0300

    Header gradiente

--- lib/src/widgets/headers_widgets.dart
+++ lib/src/widgets/headers_widgets.dart
@@ -36,7 +36,7 @@ class HeaderDiagonal extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       child: CustomPaint(
-        painter: _HeaderWavePainter(),
+        painter: _HeaderWaveGradientPainter(),
       ),
     );
   }
@@ -174,6 +174,54 @@ class _HeaderWavePainter extends CustomPainter{
     final lapiz = Paint();
     //Propiedades
     lapiz.color = Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 20;
+
+    final path = Path();
+    //dibujar Path y lapiz
+    
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.4, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.1, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    
+
+    canvas.drawPath(path, lapiz);
+    
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+
+    return true;
+  }
+
+}
+
+class _HeaderWaveGradientPainter extends CustomPainter{
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final Rect rect = Rect.fromCircle(
+     center: Offset(size.width * 0.5 ,55),
+     radius: 180
+    );
+
+    final Gradient gradient = LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [
+        Color(0xff615AAB), 
+        Color(0xffC012FF),
+        Color(0xff6D05FA),
+    ]
+    );
+
+    final lapiz = Paint()..shader = gradient.createShader(rect);
+    //Propiedades
+    //Color(0xff615AAB)
+    lapiz.color = Colors.red;
     lapiz.style = PaintingStyle.fill;
     lapiz.strokeWidth = 20;
 
