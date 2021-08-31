83af62e67166efe4d236edc652e84c81ef0f9beb
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-08-30 23:29:41 -0300

    Animaciones inicial fix

---
+++ lib/src/pages/animaciones_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AnimacionesPage extends StatelessWidget {
+  const AnimacionesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  
+
+  @override
+  _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangulo();
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+    return Container(
+       width: 70,
+       height: 70,
+       decoration: BoxDecoration(
+         color: Colors.blue
+       ),
+     );
+   }
+}
