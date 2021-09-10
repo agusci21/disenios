8587229797f6c12e5f54d03fab8679085d904c11
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-09-10 18:15:21 -0300

    emergency landing header iconos

--- lib/src/widgets/headers_widgets.dart
+++ lib/src/widgets/headers_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HeaderCuadrado extends StatelessWidget {
   
@@ -252,6 +253,37 @@ class _HeaderWaveGradientPainter extends CustomPainter{
 }
 
 class IconHeader extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final colorBlanco = Colors.white.withOpacity(0.7);
+    return Stack(
+      children: [
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(FontAwesomeIcons.plus, size: 250,color: Colors.white.withOpacity(0.2))
+        ),
+        Column(
+          children: [
+            SizedBox(height: 50, width: double.infinity,),
+            Text('Haz Solicitado', style: TextStyle(fontSize: 20, color: colorBlanco), ),
+            SizedBox(height: 20,),
+            Text('Asistencia Medica',style: TextStyle(fontSize: 25, color: colorBlanco, fontWeight: FontWeight.bold),),
+            SizedBox(height: 20,),
+            FaIcon(FontAwesomeIcons.plus, size: 85,color: Colors.white)
+          ],
+        )
+      ]
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
+  const _IconHeaderBackground({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
