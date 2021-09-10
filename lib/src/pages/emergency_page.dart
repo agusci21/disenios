55c5a6f8b7b0b5d4f182f2e339895e250bcff2b7
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-09-10 18:45:20 -0300

    emergency landing boton gordo inicio

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:disenios_01/src/widgets/boton_gordo_widget.dart';
 import 'package:disenios_01/src/widgets/headers_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -8,14 +9,27 @@ class EmergenyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.plus,
-        titulo: 'Asistencia medica',
-        subtitulo: 'Haz solicitado',
-        color1: Color(0xff526bf6),
-        color2: Color(0xff67ACF2),
-
+      body: Center(
+        child: BotonGordo(),
       )
    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.plus,
+      titulo: 'Asistencia medica',
+      subtitulo: 'Haz solicitado',
+      color1: Color(0xff526bf6),
+      color2: Color(0xff67ACF2),
+
+    );
   }
 }
